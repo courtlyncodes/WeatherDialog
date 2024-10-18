2a958ce8573505f7cdb245453d0d97c8ba9d70ba
Courtlyn Boykin <boykin.courtlyn@gmail.com>
2024-10-18 14:28:30 -0400

    Initial Commit

---
+++ WeatherDialog/WeatherDialogApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDialogApp.swift
+//  WeatherDialog
+//
+//  Created by Courtlyn on 10/18/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDialogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
